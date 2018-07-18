7621f771bb10f9c42150d744400c7a9afda08517
Carlo Panzi <carlo.panzi@gmail.com>
2018-07-18 09:59:37 +0200

    First draft

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # did
+
+A tool to keep track of the activities done, inspired by this post: [did.txt file][blog].
+
+
+
+## Usage
+
+
+
+```sh
+$ did "Writing the README"
+```
+
+This will produce a `~/did.txt` file with the logs appended.
+
+[blog]: https://theptrk.com/2018/07/11/did-txt-file/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+extern crate chrono;
+extern crate clap;
+
+use chrono::Local;
+use clap::{App, Arg};
+use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("did")
+        .version("0.1.0")
+        .about("Helps me remember what I did")
+        .author("Carlo <carlo.panzi@gmail.com>")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let date = Local::now();
+    let home_path = env::home_dir().unwrap();
+    let did_file = home_path.join("did.txt");
+
+    let date_str = date.format("[%Y-%m-%d %H:%M:%S]");
+    let did_str = matches.value_of("INPUT").unwrap();
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(did_file)
+        .unwrap();
+
+    let line = format!("{} - {}", date_str, did_str);
+
+    if let Err(e) = writeln!(file, "{}", line) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+
+    println!("{}", line);
 }
